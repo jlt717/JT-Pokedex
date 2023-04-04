282140663ad19caf433812c3e6149fac8e8379cd
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-04-04 17:43:43 -0500

    Added Pokemon
    Added an array of 5 Pokemon with 4 features for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-alert('Hello World');
+let pokemon1 = { name: "Lickilicky", height: "5'/7", type: "Normal", category: "Licking" };
+let pokemon2 = { name: "Caterpie", height: "1'/0", type: "Bug", category: "Worm" };
+let pokemon3 = { name: "Vileplume", height: "3'/11", type: ["Grass", "Poison"], category: "Flower" };
+let pokemon4 = { name: "Pikachu", height: "1'/4", type: "Electric", category: "Mouse" };
+let pokemon5 = { name: "Jigglypuff", height: "1'/8", type: ["Normal", "Fairy"], category: "Balloon" };
 
-let favoriteFood= 'Nachos';
-document.write(favoriteFood);
+let pokemonList =
+    [{ name: "Lickilicky", height: "5'/7", type: "Normal", category: "Licking" },
+    { name: "Caterpie", height: "1'/0", type: "Bug", category: "Worm" },
+    { name: "Vileplume", height: "3'/11", type: ["Grass", "Poison"], category: "Flower" },
+    { name: "Pikachu", height: "1'/4", type: "Electric", category: "Mouse" },
+    { name: "Jigglypuff", height: "1'/8", type: ["Normal", "Fairy"], category: "Balloon" }]
+console.log(pokemonList);
