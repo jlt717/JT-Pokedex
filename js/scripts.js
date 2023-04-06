e96ae4e02f8d14042d4647a74e6052d247bb12ee
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-04-06 12:47:58 -0500

    Adjusted height values
    Changed height values of Pokémon from strings to numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 let pokemonList =
-    [{ name: "Lickilicky", height: "5 feet 7 inches", type: "Normal", category: "Licking" },
-    { name: "Vileplume", height: "3 feet 11 inches", type: ["Grass", "Poison"], category: "Flower" },
-    { name: "Pikachu", height: "1 foot four inches", type: "Electric", category: "Mouse" },
-    { name: "Caterpie", height: "1 foot", type: "Bug", category: "Worm" },
-    { name: "Jigglypuff", height: "1 foot eight inches", type: ["Normal", "Fairy"], category: "Balloon" }];
+    [{ name: "Lickilicky", height: 5.6, type: "Normal", category: "Licking" },
+    { name: "Vileplume", height: 3.9, type: ["Grass", "Poison"], category: "Flower" },
+    { name: "Pikachu", height: 1.3, type: "Electric", category: "Mouse" },
+    { name: "Caterpie", height: 1, type: "Bug", category: "Worm" },
+    { name: "Jigglypuff", height: 1.7, type: ["Normal", "Fairy"], category: "Balloon" }];
 
 //for loop for pokemonList with conditional of heights returning a specific message
 document.write("<header>" + "Pokédex" + "</header>")
-for (let i = 0; i < pokemonList.length; i++)
-    if (pokemonList[i].height > "5 feet") {
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 5) {
         document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "Wow, that's big! </div>")
-    } else if (pokemonList[i].height > "1 foot" && pokemonList[i].height < "5 feet") {
+    } else if (pokemonList[i].height > 1 && pokemonList[i].height < 5) {
         document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "This one is medium. </div>")
     } else {
         document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "Just a pipsqueak! </div>")
     }
+}
 //Pokeball image
     document.write("<img src= img/pokeball.svg>")
