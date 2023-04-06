2b0b58006908942ff8cd4417b24a581f52acc7e5
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-04-06 11:38:47 -0500

    Created loop, conditional
    Created a loop with a conditional and added an image

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-let pokemon1 = { name: "Lickilicky", height: "5'/7", type: "Normal", category: "Licking" };
-let pokemon2 = { name: "Caterpie", height: "1'/0", type: "Bug", category: "Worm" };
-let pokemon3 = { name: "Vileplume", height: "3'/11", type: ["Grass", "Poison"], category: "Flower" };
-let pokemon4 = { name: "Pikachu", height: "1'/4", type: "Electric", category: "Mouse" };
-let pokemon5 = { name: "Jigglypuff", height: "1'/8", type: ["Normal", "Fairy"], category: "Balloon" };
-
 let pokemonList =
-    [{ name: "Lickilicky", height: "5'/7", type: "Normal", category: "Licking" },
-    { name: "Caterpie", height: "1'/0", type: "Bug", category: "Worm" },
-    { name: "Vileplume", height: "3'/11", type: ["Grass", "Poison"], category: "Flower" },
-    { name: "Pikachu", height: "1'/4", type: "Electric", category: "Mouse" },
-    { name: "Jigglypuff", height: "1'/8", type: ["Normal", "Fairy"], category: "Balloon" }]
-console.log(pokemonList);
+    [{ name: "Lickilicky", height: "5 feet 7 inches", type: "Normal", category: "Licking" },
+    { name: "Vileplume", height: "3 feet 11 inches", type: ["Grass", "Poison"], category: "Flower" },
+    { name: "Pikachu", height: "1 foot four inches", type: "Electric", category: "Mouse" },
+    { name: "Caterpie", height: "1 foot", type: "Bug", category: "Worm" },
+    { name: "Jigglypuff", height: "1 foot eight inches", type: ["Normal", "Fairy"], category: "Balloon" }];
+
+//for loop for pokemonList with conditional of heights returning a specific message
+document.write("<header>" + "Pokédex" + "</header>")
+for (let i = 0; i < pokemonList.length; i++)
+    if (pokemonList[i].height > "5 feet") {
+        document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "Wow, that's big! </div>")
+    } else if (pokemonList[i].height > "1 foot" && pokemonList[i].height < "5 feet") {
+        document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "This one is medium. </div>")
+    } else {
+        document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")" + " " + "Just a pipsqueak! </div>")
+    }
+//Pokeball image
+    document.write("<img src= img/pokeball.svg>")
