315df195a27a9d4264c8d6eba460c7c10c8ca006
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-04-11 10:37:08 -0500

    Updated functions for ease of reading

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-//IIFE
-let pokemonRepository = (function() {
+//IIFE and getAll and add functions
+const pokemonRepository = (function() {
     let pokemonList =
     [{ name: "Lickilicky", height: 5.6, type: "normal", category: "licking" },
     { name: "Vileplume", height: 3.9, type: ["grass", "poison"], category: "flower" },
@@ -7,16 +7,19 @@ let pokemonRepository = (function() {
     { name: "Caterpie", height: 1, type: "bug", category: "worm" },
     { name: "Jigglypuff", height: 1.7, type: ["normal", "fairy"], category: "balloon" }];
 
-return {
-    add: function(pokemon) {
-    pokemonList.push(pokemon);
-},
-getAll: function() {
-    return pokemonList;
-}
-    };
-}) ();
+    function getAll() {
+       return pokemonList;
+    }
 
+    function add(pokemon) {
+       pokemonList.push(pokemon);
+    }
+
+    return {
+       add: add,
+       getAll: getAll 
+    }
+}) ();
 
 document.write("<header>" + "Pokédex" + "</header>")
 
