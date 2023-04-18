9723cb823b490cf39e497d70b5009ffbc7f4a1bf
jlt717 <128320420+jlt717@users.noreply.github.com>
2023-04-18 12:38:44 -0500

    Update scripts.js
    Added some modal code

--- js/scripts.js
+++ js/scripts.js
@@ -59,16 +59,68 @@ const pokemonRepository = (function () {
 
    function showDetails(item) {
       loadDetails(item).then(function () {
-         console.log(item);
+         showModal(item);
       });
    }
+
+   let modalContainer = document.querySelector('#modal-container');
+  
+ function showModal(title, text, image) {
+  modalContainer.innerHTML = '';
+  
+let modal = document.createElement('div');
+  modal.classList.add ('modal');
+  
+// let closeButtonElement = document.createElement('button');
+//   closeButtonElement.classList.add('modal-close');
+//   closeButtonElement.innerText = 'Close';
+//   closeButtonElement.addEventListener('click', hideModal);
+
+// let titleElement = document.createElement('h1');
+//   titleElement.innerText = title;
+  
+// let contentElement = document.createElement('p');
+//   contentElement.innerText = text;
+
+// modal.appendChild(closeButtonElement);
+// modal.appendChild(titleElement);
+// modal.appendChild(contentElement);
+// modalContainer.appendChild(modal);
+  
+  
+// modalContainer.classList.add('is-visible');
+// }
+
+// function hideModal() {
+//   modalContainer.classList.remove('is-visible');
+// }
+
+// window.addEventListener('keydown', (e) => {
+//   if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {hideModal();
+// }
+// });
+  
+// modalContainer.addEventListener('click', (e) => {
+//   let target = e.target;
+//   if (target === modalContainer) {
+//     hideModal();
+//   }
+// });
+  
+// document.querySelector('#show-modal').addEventListener
+//   ('click', () => {
+//   showModal('Modal title', 'This is the modal content');
+// });
+  
+
    return {
       add: add,
       getAll: getAll,
       addListItem: addListItem,
       loadList: loadList,
       loadDetails: loadDetails,
-      showDetails: showDetails
+      showDetails: showDetails,
+      showModal: showModal
    };
 
 })();
